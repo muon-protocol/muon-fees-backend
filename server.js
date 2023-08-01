824ea21ded6d63c44347031350a3dd89c768bf08
farazfo@yahoo.com <11iloveu005>
2023-08-01 10:12:09 +0330

    dockerize

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const {createClient} = require("redis");
 
-const PORT = process.env.SERVER_PORT || 3000;
+const PORT = process.env.SERVER_PORT || 3002;
 
 const app = express();
 app.use(bodyParser.json());
